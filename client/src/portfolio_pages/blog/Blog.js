5b29495a0f513a8eca53e93eb0d89bb2e5936295
faizansafwan <faizansafwan075@gmail.com>
2024-07-03 14:28:37 +0530

    commented

--- client/src/portfolio_pages/blog/Blog.js
+++ client/src/portfolio_pages/blog/Blog.js
@@ -7,6 +7,7 @@ import blog3 from '../blog/blog3.png'
 
 export default function Blog() {
 
+    // List of blog
     const blogList = [
         {src: blog1, alt: 'Blog 1', blogHead: 'Marketing analysis for online business and their operational management', desc: 'Nowadays online market is trending all over the world. Most of the people over 18 ages are started online business without gender differentiation, it may be small and large business...', url: 'https://medium.com/@faizansafwan075/marketing-analysis-for-online-business-and-their-operational-management-1f58965eb92'},
         {src: blog2, alt: 'Blog 2', blogHead: 'why do we need exercises or sports?', desc: 'Exercise and sports are most important part of our life. It will help to control our mind, body and health well, as well as it will be felt our mind better and reduce the diseases...', url: 'https://medium.com/@faizansafwan075/why-do-we-need-exercises-or-sports-ca1c62d329a9'},
@@ -14,12 +15,15 @@ export default function Blog() {
     ]
 
     return(
-        <div>
 
+        // Blog Container
+        <div>
+            {/* Blog heading */}
             <div className="blog-head">
                 <h3 className="sub-heading">Blog</h3>
             </div>
 
+            {/* Design of each blog */}
             <div className="d-flex flex-wrap blog-gallery">
                 {
                     blogList.map((content, index) => (
